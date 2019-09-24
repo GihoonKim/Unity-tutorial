82f51fc7d65d5a8e543985ce34388a326ffa807f
GihoonKim <rlgns4861@hanyang.ac.kr>
2019-09-24 23:17:30 +0900

    Add Survival-Shooter
    Final Unity tutorial projects
    
     - learn about cast function, detection, spawn, etc.

---
+++ survival-shooter/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    //public GameObject enemy1;
+    //public GameObject enemy2;
+
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    void Start ()
+    {
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+        //int enemyindex = Random.Range(0,3);
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+        /*
+        if (enemyindex == 1)
+        {
+
+        }
+        else if (enemyindex == 2)
+        {
+            Instantiate(enemy1, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+        }
+        else
+        {
+            Instantiate(enemy2, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+        }
+        */
+    }
+}
